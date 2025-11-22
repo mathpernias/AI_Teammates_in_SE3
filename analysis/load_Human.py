f4bb1e8590ab3b34ac474c8c608a909db1fd577c
philippebertrand22 <90271955+philippebertrand22@users.noreply.github.com>
2025-11-22 17:28:00 -0500

    added print out to CSV

--- analysis/load_Human.py
+++ analysis/load_Human.py
@@ -143,4 +143,4 @@ if __name__ == "__main__":
     
     #THE API IS LIMITED SO ONLY RUN ON A FEW ROWS FOR TESTING
     enriched = enrich_df_with_pr_stats(limited_df, url_column="html_url")
-    print(enriched)
+    enriched.to_csv("human_prs_enriched.csv", index=True)
